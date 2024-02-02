f923416577d1de1f77da688b60ef6eab39fea911
antoniolaikauf <antonio.laikauf00@gmail.com>
2024-02-02 13:48:54 +0100

    add: aggiunto construct

--- index.php
+++ index.php
@@ -4,12 +4,23 @@ class movie
     public $nome;
     public $data_uscita;
     public $genere;
+    // construct
+    function __construct($nome, $data_uscita, $genere)
+    {
+        $this->nome = $nome;
+        $this->data_uscita = $data_uscita;
+        $this->genere = $genere;
+    }
 }
+// dichiarazione funzione dentro alla classe
+$films = new movie('pirati dei caraibi', '02-01-2020', 'divertente');
 
-$films = new movie;
+var_dump($films);
+echo "<br> -----------";
 // variabili di instanza 
 var_dump($films->nome = 'ritorno al futuro');
 echo "<br>";
-var_dump($films->nome = '02-02-2024');
+var_dump($films->data_uscita = '02-02-2024');
 echo "<br>";
-var_dump($films->nome = 'scientifico');
+var_dump($films->genere = 'scientifico');
+

ca2189035e0dc37d80853bf7183d55d2c7b575a3
antoniolaikauf <antonio.laikauf00@gmail.com>
2024-02-02 13:39:23 +0100

    add: aggiunto variabili d'instanza

--- index.php
+++ index.php
@@ -1,2 +1,15 @@
 <?php
-echo ' funziono';
+class movie
+{
+    public $nome;
+    public $data_uscita;
+    public $genere;
+}
+
+$films = new movie;
+// variabili di instanza 
+var_dump($films->nome = 'ritorno al futuro');
+echo "<br>";
+var_dump($films->nome = '02-02-2024');
+echo "<br>";
+var_dump($films->nome = 'scientifico');
